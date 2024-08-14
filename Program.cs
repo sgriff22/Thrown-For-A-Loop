473d5c4a52a5b4890b0c0b2886f412a7cb88b0fd
Stacy Griffy <sgriffy2@gmail.com>
2024-08-14 13:18:24 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,162 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<Product> products = new List<Product>()
+{
+    new Product()
+    { 
+        Name = "Football", 
+        Price = 15.00M, 
+        Sold = false,
+        StockDate = new DateTime(2022, 10, 20),
+        ManufactureYear = 2010,
+        Condition = 4.2
+    },
+    new Product() 
+    { 
+        Name = "Hockey Stick", 
+        Price = 12.50M, 
+        Sold = false,
+        StockDate = new DateTime(2023, 5, 12),
+        ManufactureYear = 2022,
+        Condition = 3.8
+    },
+    new Product()
+    {
+        Name = "Basketball",
+        Price = 10.75M,
+        Sold = false,
+        StockDate = new DateTime(2024, 8, 13),
+        ManufactureYear = 2023,
+        Condition = 4.5
+    },
+    new Product()
+    {
+        Name = "Cleats",
+        Price = 20.99M,
+        Sold = true,
+        StockDate = new DateTime(2024, 7, 30),
+        ManufactureYear = 2021,
+        Condition = 3.4
+    }
+};
+string greeting = @"Welcome to Thrown For a Loop
+Your one-stop shop for used sporting equipment";
+Console.WriteLine(greeting);
+
+string choice = null;
+while (choice != "0")
+{
+    Console.WriteLine(@"Choose an option:
+                        0. Exit
+                        1. View All Products
+                        2. View Product Details
+                        3. View Latest Products");
+    choice = Console.ReadLine();
+    if (choice == "0")
+    {
+        Console.WriteLine("Goodbye!");
+    }
+    else if (choice == "1")
+    {
+        ListProducts();
+    }
+    else if (choice == "2")
+    {
+        ViewProductDetails();
+    }
+    else if (choice == "3")
+    {
+        ViewLatestProducts();
+    }
+}
+
+
+void ViewProductDetails()
+{
+    ListProducts();
+
+    Product chosenProduct = null;
+
+    while (chosenProduct == null)
+    {
+        Console.WriteLine("Please enter a product number: ");
+        try
+        {
+            int response = int.Parse(Console.ReadLine().Trim());
+            chosenProduct = products[response - 1];
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Please type only integers!");
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Please choose an existing item only!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            Console.WriteLine("Do Better!");
+        }
+    }
+
+    DateTime now = DateTime.Now;
+    TimeSpan timeInStock = now - chosenProduct.StockDate;
+    Console.WriteLine(@$"You chose: 
+    {chosenProduct.Name}, which costs {chosenProduct.Price} dollars.
+    It is {now.Year - chosenProduct.ManufactureYear} years old.
+    It {(chosenProduct.Sold ? "is not available" : $"has bean in stock for {timeInStock.Days} days.")}
+    Condition rating: {chosenProduct.Condition}"); 
+}
+
+void ListProducts()
+{
+    decimal totalValue = 0.0M;
+    foreach (Product product in products)
+    {
+        if (!product.Sold)
+        {
+            totalValue += product.Price;
+        }
+    }
+    Console.WriteLine($"Total inventory value: ${totalValue}");
+    Console.WriteLine("Products:");
+    for (int i = 0; i < products.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {products[i].Name}");
+    }
+}
+
+
+void ViewLatestProducts()
+{
+    // create a new empty List to store the latest products
+    List<Product> latestProducts = new List<Product>();
+    // Calculate a DateTime 90 days in the past
+    DateTime threeMonthsAgo = DateTime.Now - TimeSpan.FromDays(90);
+    //loop through the products
+    foreach (Product product in products)
+    {
+        //Add a product to latestProducts if it fits the criteria
+        if (product.StockDate > threeMonthsAgo && !product.Sold) {
+            latestProducts.Add(product);
+        }
+    }
+    // print out the latest products to the console 
+    for (int i = 0; i < latestProducts.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {latestProducts[i].Name}");
+    }
+}
+
+
+
+
+
+// if (string.IsNullOrEmpty(response))
+// {
+//     Console.WriteLine("You didn't choose anything!");
+// }
+// else
+// {
+//     Console.WriteLine($"You chose: {response}");
+// }
